cadeebc92bb7a32ef90fa81ad4a6bfe71f092df5
Jahid Hasan <jahidhasan2856@gmail.com>
2022-09-29 02:01:22 +0600

    missing file

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+app.get("/download", async (req, res) => {
+  try {
+    const url = req.query.url;
+    const videoId = await ytdl.getURLVideoID(url);
+    const metInfo = await ytdl.getInfo(url);
+    let data = {
+      url: "https://www.youtube.com/embed/" + videoId,
+      info: metInfo.formats,
+    };
+    return res.send(data);
+  } catch (err) {
+    return res.status(500);
+    console.log(err);
+  }
+});
+app.listen(4000, () => {
+  console.log("Server running on PORT :4000");
+});
