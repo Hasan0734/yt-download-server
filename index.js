4285de9b63681b966f8108a55fce116c842d6141
Jahid Hasan <jahidhasan2856@gmail.com>
2022-10-03 00:24:13 +0600

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const cors = require("cors");
 const app = express();
+
+
 app.use(cors());
 
 app.get("/download", async (req, res) => {
